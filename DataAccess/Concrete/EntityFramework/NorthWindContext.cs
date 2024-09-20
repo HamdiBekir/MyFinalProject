f0f18bc997bc69d1ca67f3cfc4434e2de350af7c
Hamdi Bekir Yılmaz <hamdibekiryilmaz0@gmail.com>
2024-09-21 00:23:52 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/NorthWindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //context - Db tablolorı ile proje classlarınıbağlamak
+    public class NorthWindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=NorthWind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ProductId = 1, CategoryId = 1, ProductName = "Bilgisayar", UnitPrice = 15000, UnitsInStock = 5},
+                new Product{ProductId = 2, CategoryId = 1, ProductName = "Klavye", UnitPrice = 2500, UnitsInStock = 13},
+                new Product{ProductId = 3, CategoryId = 2, ProductName = "Fare", UnitPrice = 1300, UnitsInStock = 2},
+                new Product{ProductId = 4, CategoryId = 2, ProductName = "Telefon", UnitPrice = 7150, UnitsInStock = 25},
+                new Product{ProductId = 5, CategoryId = 2, ProductName = "Saat", UnitPrice = 1185, UnitsInStock = 11}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün Id'sine sahip olan listedeki ürün idsini bul.
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
